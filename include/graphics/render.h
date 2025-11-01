badf33b100fe4311b5c54945bd54ede4bc1d1d6c
Danil Usoltsev <danilusoltsev05@gmail.com>
2025-11-01 13:38:17 +0300

    Format

--- include/graphics/render.h
+++ include/graphics/render.h
@@ -11,6 +11,12 @@ Sprite load_sprite_scaled(const char *path, int scale);
 void free_sprite(Sprite *sprite);
 
 // Drawing functions
-void draw_sprite(uint32_t *framebuffer, int fb_width, int fb_height, Sprite *sprite, int x, int y, bool flip_horizontal);
+void draw_sprite(uint32_t *framebuffer,
+    int fb_width,
+    int fb_height,
+    Sprite *sprite,
+    int x,
+    int y,
+    bool flip_horizontal);
 
 #endif
