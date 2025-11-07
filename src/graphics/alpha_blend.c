e558c517eac4a1f2a1bb37e9d82225e0839943fe
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2025-11-07 22:40:39 +0300

    perf: optimize alpha blend

--- src/graphics/alpha_blend.c
+++ src/graphics/alpha_blend.c
@@ -1,25 +1,15 @@
 #include "alpha_blend.h"
 
-// Accepts ARGB src and dst colors, returns ARGB blended color
-uint32_t alpha_blend(uint32_t src, uint32_t dst) {
-  uint8_t src_a = (src >> 24) & 0xFF;
-  if (src_a == 0) return dst;
+// Accepts ARGB top and bot colors, returns ARGB blended color
+inline uint32_t alpha_blend(uint32_t top, uint32_t bot) {
+  uint32_t top_a = (top >> 24);
+  if (top_a == 0) return bot;
+  if (top_a == 255) return top;
 
-  if (src_a == 255) {
-    return src;
-  } else {
-    uint8_t src_r = (src >> 16) & 0xFF;
-    uint8_t src_g = (src >> 8) & 0xFF;
-    uint8_t src_b = src & 0xFF;
+  uint32_t inv_a = 255 - top_a;
 
-    uint8_t dst_r = (dst >> 16) & 0xFF;
-    uint8_t dst_g = (dst >> 8) & 0xFF;
-    uint8_t dst_b = dst & 0xFF;
-
-    uint8_t out_r = (src_r * src_a + dst_r * (255 - src_a)) / 255;
-    uint8_t out_g = (src_g * src_a + dst_g * (255 - src_a)) / 255;
-    uint8_t out_b = (src_b * src_a + dst_b * (255 - src_a)) / 255;
-
-    return 0xFF000000 | (out_r << 16) | (out_g << 8) | out_b;
-  }
+  // red and blue channels together for optimization
+  uint32_t rb = (((top & 0x00FF00FF) * top_a + (bot & 0x00FF00FF) * inv_a) >> 8) & 0x00FF00FF;
+  uint32_t g = (((top & 0x0000FF00) * top_a + (bot & 0x0000FF00) * inv_a) >> 8) & 0x0000FF00;
+  return 0xFF000000 | rb | g;
 }
